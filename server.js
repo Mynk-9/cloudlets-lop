b01f6ff600254083d4d4c9bcd97c4c887c07f47e
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2022-04-28 22:34:06 +0530

    Added queuing logic to server

--- server.js
+++ server.js
@@ -8,21 +8,33 @@ const io = new Server(PORT);
 
 let intervals = [];
 let clients = {};
+let dataQueue = [];
+const checkLowStorageUsageClients = () => {
+    let lowStorageUsageClients = [];
+    for (const clientId in clients) {
+        const ratio = clients[clientId].storageRatio;
+        if (ratio != null) lowStorageUsageClients.push(ratio);
+    }
+    return lowStorageUsageClients;
+};
 
 io.on('connection', socket => {
     console.log('user connected', socket.id);
     clients[socket.id] = {};
 
     socket.on('storage_info', data => {
-        // console.log(
-        //     'storage info update for',
-        //     socket.id,
-        //     'status:',
-        //     data.state
-        // );
         if (data.state === 'HIG') {
-            console.log('HIG storage usage in ', socket.id);
-            socket.emit('high_storage_send_data');
+            console.log('HIG storage usage in', socket.id);
+            if (checkLowStorageUsageClients().length > 0)
+                socket.emit('high_storage_send_data');
+        } else if (dataQueue.length !== 0) {
+            const queuedData = dataQueue[0].data;
+            const queuedFromClient = dataQueue[0].fromClient;
+            console.log(
+                `LOW/MID storage usage in ${socket.id}, sending queued data of ${queuedFromClient}`
+            );
+            socket.emit('receive_data', queuedData);
+            dataQueue = dataQueue.slice(1);
         }
         clients[socket.id] = {
             storageState: data.state,
@@ -40,10 +52,23 @@ io.on('connection', socket => {
                 lowestStorageClient = clientId;
             }
         }
+
         if (lowestStorageClient === null) {
-            console.log('Error: no information on client storage available');
+            console.log(
+                'Error: no information on client storage available. Adding data to data queue.'
+            );
+            dataQueue.push({
+                data: data,
+                fromClient: socket.id,
+            });
         } else if (clients[lowestStorageClient].storageState === 'HIG') {
-            console.log('Error: no client with LOW or MID storage available');
+            console.log(
+                'Error: no client with LOW or MID storage available. Adding data to data queue.'
+            );
+            dataQueue.push({
+                data: data,
+                fromClient: socket.id,
+            });
         } else {
             console.log(
                 'Transferring files from',
